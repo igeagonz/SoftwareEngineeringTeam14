96f9cf05b3c32227232d3ba1ee18072cb20acd4c
Ignacio Gea <igea.17@gmail.com>
2016-04-04 11:37:11 -0500

    adding BackgroundTaskLogin file to branch

---
+++ Sparkit/app/src/main/java/com/sparkit/sparkit/BackgroundTaskLogin.java
@@ -0,0 +1,98 @@
+package com.sparkit.sparkit;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+/**
+ * Created by nacho on 3/11/16.
+ */
+public class BackgroundTaskLogin{
+
+    Context ctx;
+    //AlertDialog alertDialog;
+    BackgroundTaskLogin(Context ctx){
+        this.ctx = ctx;
+
+    }
+    protected void doInBackground(String emailLogin, String passwordLogin, LoginActivity loginActivity){
+
+        String login_url = "http://130.184.99.197/login.php";
+        //String method  = params[0];
+        String email = emailLogin;
+        String password = passwordLogin;
+
+            try {
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS,"UTF-8"));
+
+                String data = URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8") + "&" +
+                        URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+
+                InputStream IS = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(IS, "iso-8859-1"));
+                String result = "";
+                String line = "";
+
+                while((line = bufferedReader.readLine()) != null){
+                    result += line;
+                }
+
+                bufferedReader.close();
+                IS.close();
+                httpURLConnection.disconnect();
+
+
+                if(!result.equals("Incorrect username and password... Try again")){
+                    loginActivity.goToMain();
+                }
+
+                Toast toast = Toast.makeText(ctx, result, Toast.LENGTH_SHORT);
+                toast.show();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+
+    }
+
+
+    protected void onPostExecute(String result){
+
+
+        /*
+        activity.updateStatus(result);
+        */
+
+    }
+
+}
+
